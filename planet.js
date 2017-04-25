9d00fbc85ca79d230aad909e01b8a907f05f3b93
Tyler Mechura <tjm471@nyu.edu>
2017-04-25 17:38:39 -0400

    Comments added, size changed
    added some functionality comments for later
    
    changed the size to something more reasonable so the trees won't get
    blocked offscreen later

--- planet.js
+++ planet.js
@@ -1,7 +1,8 @@
 function Planet() {
-  this.colorArray = ['#AD78B2', '#FFF7DF', '#89CCBC', '#81B2A7'];
+  this.colorArray = ['#AD78B2', '#FFF7DF', '#89CCBC', '#81B2A7']; // array of colors. See URL below for portfolio scheme picker. #FAC5FF reserved for background
+  // https://color.adobe.com/Creative-Coding-Final-Project-Colors-color-theme-9496388/
   this.planetColor = this.colorArray[int(random(0, 3))];
-  this.size = random(width / 1.8, height / 1.8);
+  this.size = random(width / 3, height / 3);
   this.center = createVector(width / 2, height / 2);
   this.strokeWeight = random(5, 20);
 
@@ -11,7 +12,6 @@ function Planet() {
     fill(this.planetColor);
     ellipse(this.center.x, this.center.y, this.size, this.size);
     // more will be added later. This is part of the 'skeleton structure'
-    // I want to finish the fuctionality before I start adding style
   }
   this.update = function () {
 
@@ -20,9 +20,13 @@ function Planet() {
 
   // what will show that the planet is rotating?
   // the cloud? The tree? will the Trees be the ones moving and not the actual ellipse that makes the plannet?
+  // how do I 'anchor' an object to a point on the planet?
   // What will the 'roots' be a part of? the planet?
   this.rotate = function (direction) {
-    //    var dirVector = 
+    // LEFT_ARROW = Rotate counterclockwise
+    // RIGHT_ARROW = Rotate clockwise
+    // slowly rotates on it's own?
+
   }
 
   this.changePlanet = function () {
