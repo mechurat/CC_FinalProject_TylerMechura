348993fa0b063ee3c162db888a54d63660d9794f
Tyler Mechura <tjm471@nyu.edu>
2017-04-27 16:45:05 -0400

    Name Changes
    Changed 'dropPoint' to 'location' for naming conventions to make code
    easier to read.

--- seed.js
+++ seed.js
@@ -5,7 +5,7 @@ function Seed() {
   ////////////////////
 
   // initial drop point of the seed.
-  this.dropPoint = createVector(mouseX, mouseY);
+  this.location = createVector(mouseX, mouseY);
   var p1 = {
     x: mouseX,
     y: mouseY
@@ -25,13 +25,13 @@ function Seed() {
 
   // initializing the vectors to create the velocity within a reasonable range
   var center = createVector(width / 2, height / 2);
-  var moveToCenter = p5.Vector.sub(center, this.dropPoint);
+  var moveToCenter = p5.Vector.sub(center, this.location);
   // velocity from MoveToCenter vector re-mapped
   // previously had the mapping to -10 -> 10, but it was off-center at certain angles
   // now everything is in calculations of width/height
   // these are defined as windowWidth and windowHeight in 'sketch.js'
-  var velX = map(moveToCenter.x, width * -1, width, width / 50 * -1, width / 50);
-  var velY = map(moveToCenter.y, height * -1, height, height / 50 * -1, height / 50);
+  var velX = map(moveToCenter.x, width * -1, width, width / 80 * -1, width / 80);
+  var velY = map(moveToCenter.y, height * -1, height, height / 80 * -1, height / 80);
   // final velocity of seed from mouse point click
   this.velocity = createVector(velX, velY);
 
@@ -42,10 +42,11 @@ function Seed() {
   this.show = function () {
     noStroke();
     fill('#81B2A7');
-    ellipse(this.dropPoint.x, this.dropPoint.y, 15, 15);
+    ellipse(this.location.x, this.location.y, 15, 15);
   }
   this.update = function () {
-    this.dropPoint.add(this.velocity);
+    this.location.add(this.velocity);
+    if
   }
 
   // functions needed:
