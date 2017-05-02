33ea7b0f5462882fd89ce01f74ab21e89de3110a
Tyler Mechura <tjm471@nyu.edu>
2017-05-02 09:41:19 -0400

    Breaking down the problem
    Made this.growBranch() function to simplify growing procedure

--- tree.js
+++ tree.js
@@ -35,67 +35,48 @@ function Tree(growthPoint, seedClickPoint) {
     line(this.startPos.x, this.startPos.y, this.growOut.x, this.growOut.y);
   }
   this.update = function () {
-    // define length of Branch using Euclidian Distance formula
-    var splitBranchDistance = sqrt(pow(this.growOut.x - this.startPos.x, 2) + pow(this.growOut.y - this.startPos.y, 2));
+    this.growBranch(this.startPos, this.growOut, splitTime, this.growthSpeed);
 
-    // if statement telling the tree to keep growing until it reaches beyond the distance of the split time
-    // initial tree growth
-    if (splitBranchDistance < splitTime) {
-      // grow tree
-      this.growOut.sub(this.growthSpeed);
-      console.log(splitBranchDistance + '\n' + splitTime);
-    } else {
-      // moving the origin to the point at the end of the branch
-      //      translate(this.growOut.x, this.growOut.y);
-      //      var rotateDirection = this.growthDirection.mult(theta);
-      var newStart = this.growOut.copy();
-      this.grow(newStart);
-    }
+    //      var rotation = this.growthDirection.copy();
+    //      var speed = createVector(rotation.x * .008, rotation.y * .008);
+    //      var growth = this.growOut.copy();
+    //      this.grow(growth);
   }
 
+  this.growBranch = function (start, growPoint, stopAt, speed) {
+    // calculate branchLength each time this is run
+    var branchLength = p5.Vector.dist(start, growPoint);
+
+    // exit function if BranchLength is greater than stopping point
+    if (branchLength > stopAt) {
+      console.log('Exit growBranch()');
+      return;
+    }
+
+    // increase Length of growPoint
+    growPoint.sub(speed);
+
+  }
   this.grow = function (start) {
     // using Daniel Shiffman's recursive tree example from 
     // https://processing.org/examples/tree.html
     // as REFERENCE. This code is heavily modified to fit the conditions of this project
 
     // reduce the size of 'splitTime' to a recorded amount. I like the 2/3rds from Stiffman's example, and due to the random nature of the original, this will cause the trees to vary as they are placed
-    splitTime *= .80;
+    splitTime *= .66;
     console.log('Split Time = ' + splitTime);
     // copy of parameters
-    var rotation = this.growthDirection.copy();
-    var speed = this.growthSpeed.copy();
-    var growth = start.copy();
-    // exit condition loop (so it's not infinite)
     if (splitTime > 2) {
-      // pushing to save state
-      //      push();
-      console.log('pushed');
-
+      push();
       rotation.rotate(theta);
 
-      // length of new split
-      var newSplit = p5.Vector.dist(start, growth);
-      console.log(newSplit);
-      // new branch
-      line(start.x, start.y, growth.x, growth.y);
-      if (newSplit < splitTime) {
-        console.log('newSplit < splitTime');
-        growth.sub(20);
-        console.log('growth changed: ' + growth);
-        line(start.x, start.y, growth.x, growth.y);
-        console.log('line drawn at: ' + start.x + ' ' + start.y);
-        console.log(growth);
-        if (newSplit > splitTime) {
-          this.grow(growth);
-          //          pop();
-          console.log('popped');
-        }
-
-      }
     }
+    // length of new line
+    var branchLength = p5.Vector.dist(start, growth);
+    // exit condition loop (so it's not infinite)
   }
 
-  this.growLeaves = function () {
+  this.newBranch = function () {
 
   }
 }
